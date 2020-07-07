b1a62fe29ddcc87daaa895ae7429a30165fdd041
thepuzzletechie <w3.author@gmail.com>
2020-07-07 16:02:06 +0530

    Updates

---
+++ precache-manifest.761359c1d7eab02c57b3cbf427e17fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6aad0269566548fd4180d200124cca7",
+    "url": "/reactrouterwithaxios/index.html"
+  },
+  {
+    "revision": "15bbed15671d4deebc28",
+    "url": "/reactrouterwithaxios/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c036a7d5a16fe7072a99",
+    "url": "/reactrouterwithaxios/static/js/2.de50eb30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactrouterwithaxios/static/js/2.de50eb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bbed15671d4deebc28",
+    "url": "/reactrouterwithaxios/static/js/main.179a03be.chunk.js"
+  },
+  {
+    "revision": "f5955cde69c38311d10a",
+    "url": "/reactrouterwithaxios/static/js/runtime-main.02104f97.js"
+  }
+]);
